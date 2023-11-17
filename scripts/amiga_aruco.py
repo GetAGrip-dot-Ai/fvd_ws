f28e34314e8a835c0ef46e3db4cb88f7c71d4bd7
sridevikaza <sridevikaza98@gmail.com>
2023-11-17 16:28:58 -0500

    working to publish id

--- scripts/amiga_aruco.py
+++ scripts/amiga_aruco.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 import rospy
-from aruco_msgs.msg import Marker
+from aruco_msgs.msg import MarkerArray
 from std_msgs.msg import Int16
 from geometry_msgs.msg import PoseStamped
 
-ALIGNMENT_THRESH = 0.07 # meters
+ALIGNMENT_THRESH = 0.1 # meters
 
 class AmigaArucoNode:
     
@@ -13,27 +13,29 @@ class AmigaArucoNode:
 
         # initialize values
         rospy.init_node('amiga_aruco_node', anonymous=True)
+        self.markers = MarkerArray()
         self.aligned = 0
         self.x = 0
 
         # aruco detection subscriber
-        self.aruco_sub = rospy.Subscriber("/aruco_tracker/pose", PoseStamped, self.detection_callback, queue_size=1)
-        
+        self.aruco_sub = rospy.Subscriber("/aruco_marker_publisher/markers", MarkerArray, self.detection_callback, queue_size=1)
+
         # amiga aligned publisher
         self.aligned_pub = rospy.Publisher('/amiga_aligned', Int16, queue_size=1)
 
+
     def detection_callback(self,msg):
-        self.x = msg.pose.position.x
+        self.markers = msg
 
-    def determine_aligned(self):    
-        if abs(self.x) < ALIGNMENT_THRESH and abs(self.x > 0):
-            self.aligned = 1
-        else:
-            self.aligned = 0
+    def determine_aligned(self):
+        self.aligned = 0
+        for marker in self.markers.markers:
+            x_pos = marker.pose.pose.position.x
+            if abs(x_pos) < ALIGNMENT_THRESH:
+                self.aligned = marker.id
 
-        amiga_aruco_node.aligned_pub.publish(amiga_aruco_node.aligned)
-        self.x = 0
-        # self.pose.position.x = 1000
+        self.aligned_pub.publish(self.aligned)
+        self.markers = MarkerArray()
         
 
 if __name__ == '__main__':
